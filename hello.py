a4833f806fbecdea6e9b01eb89f94b2340bfe7e7
mayanknaruka <mayanknaruka47@gmail.com>
2026-02-24 11:33:52 +0530

    recursive

--- hello.py
+++ hello.py
@@ -11,10 +11,17 @@ print('hello')
 # hello()
 
 
-def hello(n):
-    if n == 0:
-        return
-    print("hello world")
-    hello(n - 1)
+# def hello(n):
+#     if n == 0:
+#         return
+#     print("hello world")
+#     hello(n - 1)
 
-hello(10)
+# hello(10)
+
+def print(n, sum):
+    if n == 0:
+        sum = sum + 1
+        print(n-1, sum)           # recursive call with negative number
+        sum = 0
+        print (print(10, sum))    # very wrong — nested print + function returns None
