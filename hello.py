0656034b16a53ed96e2a8819b578b4d5b517d1c4
mayanknaruka <mayanknaruka47@gmail.com>
2026-02-24 23:28:51 +0530

    chai thandi ho gyi

--- hello.py
+++ hello.py
@@ -1,27 +1,5 @@
-print('hello')
+import requests
 
-# for i in range(1,10):
-#     print("hello", i)
-  
-
-# def hello():
-#     for i in range(10):
-#         print("hello")
-
-# hello()
-
-
-# def hello(n):
-#     if n == 0:
-#         return
-#     print("hello world")
-#     hello(n - 1)
-
-# hello(10)
-
-def print(n, sum):
-    if n == 0:
-        sum = sum + 1
-        print(n-1, sum)           # recursive call with negative number
-        sum = 0
-        print (print(10, sum))    # very wrong — nested print + function returns None
+# Download a web page
+response = requests.get("https://api.github.com")
+print(response.status_code)  # Should print 200
