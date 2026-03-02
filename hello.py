7fbc8559c2fc0d23bbdf051eb098b04bf55ae74c
mayanknaruka <mayanknaruka47@gmail.com>
2026-03-02 14:30:28 +0530

    def

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
-my_list = ["mayank", "iqra", "neha", 56]
+# def check_weather():
+#     temperature = 31
+#     if temperature > 30:
+#         print("It's hot!")
+#     else:
+#         print("Nice weather!")
 
-my_list[1]
+# Use the function
+# check_weather()
+
+def greet(first_name, last_name):
+    print(f"hello, {first_name}, {last_name}")
+
+greet(last_name="naruka", first_name="mayank")
