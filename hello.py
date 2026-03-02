c367a92fe57150554a0ad7f557706be78e31b1fe
mayanknaruka <mayanknaruka47@gmail.com>
2026-03-02 16:24:01 +0530

    pypy

--- hello.py
+++ hello.py
@@ -1,14 +1,1 @@
-# def check_weather():
-#     temperature = 31
-#     if temperature > 30:
-#         print("It's hot!")
-#     else:
-#         print("Nice weather!")
-
-# Use the function
-# check_weather()
-
-def greet(first_name, last_name):
-    print(f"hello, {first_name}, {last_name}")
-
-greet(last_name="naruka", first_name="mayank")
+import pandas as pd
